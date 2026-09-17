fcec0a37150b545bb6c5849392d2c5385c00ec73
perezvonish <shokilak@gmail.com>
2026-09-18 01:37:28 +0500

    fix: bootstrap

--- scenes/bootstrap/bootstrap.gd
+++ scenes/bootstrap/bootstrap.gd
@@ -5,56 +5,54 @@ extends Node
 
 @export_group("Game")
 @export var game_scene: PackedScene
+@export var player_scene: PackedScene
 
 @export_group("UI")
 @export var hud_scene: PackedScene
-
-#@export var player_scene: PackedScene
-
-#@export var death_screen_scene: PackedScene
+@export var death_screen_scene: PackedScene
+@export var player_menu_scene: PackedScene
 
 var main_menu: Control
-
 var game: Node2D
 var player: Player
+var player_menu: PlayerMenu
 var hud: UiHud
 var death_screen: CanvasLayer
-var player_menu: PlayerMenu
 
 func _ready() -> void:
 	_handle_main_menu()
-	
+
+
 func _handle_main_menu():
 	main_menu = main_menu_scene.instantiate() as Control
 	main_menu.game_start_requested.connect(_on_game_start_requested)
 	add_child(main_menu)
-	
+
 func _on_game_start_requested():
 	remove_child(main_menu)
 	main_menu.queue_free()
 	main_menu = null
 
 	game = game_scene.instantiate()
+	player = player_scene.instantiate()
+	hud = hud_scene.instantiate()
+	death_screen = death_screen_scene.instantiate()
+	death_screen.hide()
+
 	add_child(game)
-
-#	game = game_scene.instantiate() as Node2D
-#	player = player_scene.instantiate() as Player
-#	hud = hud_scene.instantiate() as UiHud
-#	death_screen = death_screen_scene.instantiate() as CanvasLayer
-
-#	add_child(game)
-#	game.add_child(player)
-#	add_child(hud)
-#	add_child(death_screen)
+	var objects := game.get_node("Objects")
+	var player_spawn := objects.get_node("PlayerSpawner") as Marker2D
+	player.position = player_spawn.position
+	objects.add_child(player)
 #	add_child(player_menu)
+	add_child(hud)
+	hud.bind_health(player.health_controller)
+	add_child(death_screen)
+
 #	player_menu.bind_inventory(player.inventory_controller)
 #	player_menu.inventory_tab.bind_transfers(ItemTransferController.new(player.inventory_controller, player.get_node("NearbyItems"), game))
 #	player_menu.closed.connect(_on_player_menu_closed)
 #
-#	var player_spawn := game.get_node("PlayerSpawn") as Marker2D
-#	player.global_position = player_spawn.global_position
-#	hud.bind_health(player.health_controller)
-#	death_screen.hide()
 #	death_screen.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 #	player.died.connect(_on_player_died)
 #	death_screen.respawn_requested.connect(_on_respawn_requested)
